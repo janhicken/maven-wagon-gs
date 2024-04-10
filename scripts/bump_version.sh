37059192fd3bfa0b94e08ea2bb25c3a0063d8422
Jan Hicken <jan.hicken@posteo.de>
2024-04-10 22:39:25 +0200

    Run shfmt on scripts

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -4,8 +4,8 @@ set -o nounset
 set -o pipefail
 
 if [[ $# -ne 2 ]]; then
-  printf 'Usage: %s FILE NEW_VERSION\n' "$0" >&2
-  exit 1
+	printf 'Usage: %s FILE NEW_VERSION\n' "$0" >&2
+	exit 1
 fi
 
 exec sed --in-place "/^PROJECT_VERSION =/c\\PROJECT_VERSION = \"$2\"" "$1"
--- scripts/cat_version.sh
+++ scripts/cat_version.sh
@@ -4,18 +4,18 @@ set -o nounset
 set -o pipefail
 
 if [[ $# -ne 1 ]]; then
-  printf 'Usage: %s FILE\n' "$0" >&2
-  exit 1
+	printf 'Usage: %s FILE\n' "$0" >&2
+	exit 1
 fi
 
 REGEX='^PROJECT_VERSION = "(.+)"$'
 readonly REGEX
 
 while read -r line; do
-  if [[ "$line" =~ $REGEX ]]; then
-    printf '%s\n' "${BASH_REMATCH[1]}"
-    exit 0
-  fi
-done < "$1"
+	if [[ "$line" =~ $REGEX ]]; then
+		printf '%s\n' "${BASH_REMATCH[1]}"
+		exit 0
+	fi
+done <"$1"
 
 exit 1
